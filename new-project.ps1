58dc9e593d350484d014acc26b05dfa88ea343fe
Martin Stühmer <me@samtrion.net>
2024-05-16 18:36:03 +0200

    feat: Created solution (#3)

--- new-project.ps1
+++ new-project.ps1
@@ -49,7 +49,7 @@ New-Project `
   -DisableTests $DisableTests `
   -DisableUnitTests $DisableUnitTests `
   -DisableIntegrationTests $DisableIntegrationTests `
-  -SolutionFile "###SOLUTION###" `
+  -SolutionFile "./Logging.Abstractions.sln" `
   -OutputDirectory (Get-Location) `
   -EnableProjectGrouping $EnableProjectGrouping `
   -DisableArchitectureTests $DisableArchitectureTests
