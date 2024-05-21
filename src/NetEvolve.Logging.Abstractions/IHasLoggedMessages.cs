47c60267d4676fd5b6d2c0a6c2f7bfe08cdca7c2
Martin Stühmer <me@samtrion.net>
2024-05-21 20:04:25 +0200

    feat: Added `IHasLoggedMessages` (#10)

---
+++ src/NetEvolve.Logging.Abstractions/IHasLoggedMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace NetEvolve.Logging.Abstractions;
+
+using System.Collections.Generic;
+
+/// <summary>
+/// Gives access to the logged messages.
+/// </summary>
+public interface IHasLoggedMessages
+{
+    /// <summary>
+    /// Gets the logged messages, in the order they were logged.
+    /// </summary>
+    IReadOnlyList<LoggedMessage> LoggedMessages { get; }
+}
